5bb84abfd871180e85a7ee79005142b4254a0195
Huỳnh Thiên Hạo <hq301119@gmail.com>
2025-08-22 22:31:53 +0700

    Add files via upload

---
+++ src/main/java/murach/data/UserDB.java
@@ -0,0 +1,12 @@
+package murach.data;
+
+import murach.business.User;
+
+public class UserDB {
+    // Cấu hình DB (m sửa lại user/pass cho đúng PostgreSQL của m)
+
+
+    public static void insert(User user) {
+        
+    }
+}
